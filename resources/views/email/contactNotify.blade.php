9f7dcf049f48546248f77fac9a7fd384ad238ce9
user <user@test0059.axcell28.idv.tw>
2020-07-16 11:57:55 +0800

    前台：諮詢廠商通知信，追加顯示詢問內容

--- resources/views/email/contactNotify.blade.php
+++ resources/views/email/contactNotify.blade.php
@@ -6,5 +6,6 @@
     <body>
         <h3>Hi! {{ $company->name }}</h3>
         <p>有厂商来信咨询您，请<a href="{{ url('/company/contact/edit', [$params['contactId']] ) }}">前往</a>查看</p>
+        <textarea style="width: 100%;height: 150px;" readonly>{{ $params['content'] }} </textarea>
     </body>
 </html>
