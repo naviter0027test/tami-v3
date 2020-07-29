59d1c377b746bb1850e2ff57211eb1dfdb575179
user <user@test0059.axcell28.idv.tw>
2020-07-29 17:08:18 +0800

    Product add col: email

--- resources/views/company/setting/index.blade.php
+++ resources/views/company/setting/index.blade.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>長鴻管理系統</title>
+        <title>管理系統</title>
         <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
         <link href='/lib/bootstrap/dist/css/bootstrap.min.css' rel='stylesheet' />
         <link href='/lib/bootstrap/dist/css/bootstrap-theme.min.css' rel='stylesheet' />
