5c1d395f295a9055c8d30d02fd26497f8c47c7e5
Lanker <naviter0027test@gmail.com>
2020-08-25 14:23:07 +0800

    首頁分類顯示調整

--- public/js/front-v3/js/index.js
+++ public/js/front-v3/js/index.js
@@ -5,6 +5,6 @@ $(document).ready(function() {
             var popupImgHeight = 100 + parseInt(listItemHeight);
             console.log(popupImgHeight);
             $('.home_div #popup01 img').css('height', popupImgHeight+'px');
-        }, 1000);
+        }, 500);
     });
 });
