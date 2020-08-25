00d9c6f9e74b69b11ae9d55aa4e75cf36260279a
user <user@test0059.axcell28.idv.tw>
2020-08-25 10:13:34 +0800

    前台首頁：顯示公司列表調整。前台公司頁：Logo、點亮資訊、右上圖的寬高調整

---
+++ public/js/front-v3/js/company.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.company_info .brand img').css('width', '500px');
+    $('.company_info .brand img').css('height', '80px');
+    $('.company_info .owl-item .box .img').css('background-repeat', 'no-repeat');
+    $('.company_info .owl-item .box .img').css('background-size', '100% 100%');
+    $('.col02 .ad img').css('width', '80%');
+    $('.col02 .ad img').css('height', '90%');
+});
---
+++ public/js/front-v3/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.animate').on('click', function() {
+        setTimeout(function() {
+            var listItemHeight = $('.home_div .list_item').css('height');
+            var popupImgHeight = 100 + parseInt(listItemHeight);
+            console.log(popupImgHeight);
+            $('.home_div #popup01 img').css('height', popupImgHeight+'px');
+        }, 1000);
+    });
+});
