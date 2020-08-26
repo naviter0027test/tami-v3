a1682f3bb6427094d1ed099afb02b081db4d9bca
Lanker <naviter0027test@gmail.com>
2020-08-26 11:15:44 +0800

    前台頁廠商點亮圖片自動輪播

--- public/js/front-v3/js/company.js
+++ public/js/front-v3/js/company.js
@@ -5,4 +5,6 @@ $(document).ready(function() {
     $('.company_info .owl-item .box .img').css('background-size', '100% 100%');
     $('.col02 .ad img').css('width', '80%');
     $('.col02 .ad img').css('height', '90%');
+
+    $( ".info_slider" ).trigger('play.owl.autoplay', [4000]);
 });
